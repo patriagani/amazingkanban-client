e2526ce62fe96e819c496d5f976d93eacfd5ddd1
patria gani <patria.gani@gmail.com>
2020-03-22 19:01:47 +0700

    done

---
+++ src/components/Board.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-card style="padding:10px;" dark :color="color">
+    <v-card-title>{{title}}</v-card-title>
+    <v-divider></v-divider>
+    <card :tasks="tasks" :status="title"></card>
+  </v-card>
+</template>
+
+<script>
+  import card from '../components/Card'
+
+  export default {
+    name: 'Board',
+
+    components: {
+      card
+    },
+
+    props: ['color', 'title', 'tasks'],
+
+    data: () => ({
+      
+    }),
+  }
+</script>
